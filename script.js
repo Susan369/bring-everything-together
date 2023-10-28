e08b51123227be8c6256e8640420ddc5df901d53
Admin Local <suzan369holland@gmail.com>
2023-10-29 01:38:02 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("its coffee time!!!");
+
+function change(){
+    document.getElementById('change').innerHTML='Do You Like Cookies?'
+}
+
+
+const n = 5;
+for (let i = 1; i <= n; i++) {
+    console.log(`i love coffee.`);
+}
+
